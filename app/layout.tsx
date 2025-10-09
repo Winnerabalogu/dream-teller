f2056d90e43f2d7d4d4669ea0cd85fbae0aa388f
Abalogu Winner <wabalogu@gmail.com>
2025-10-09 02:27:59 +0100

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,12 @@ import { Toaster } from 'sonner';
 export const metadata: Metadata = {
   title: 'Dream Tale - Interpret Your Dreams',
   description: 'Explore your subconscious mind. Discover hidden patterns. Unlock personal insights.',
+ icons: {
+     icon: [{ url: "/favicon.ico" }, { url: "/favicon1.svg", type: "image/svg+xml" }],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -18,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="apple-mobile-web-app-title" content="Arterna" />
       <body>
         <CloudBackground />
         <div className="flex min-h-screen">
